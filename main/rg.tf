15c133bf469b536dbe991de58b6c64f0bc275302
Chirag Panchal <chirag.panchal@electrolux.com>
2023-08-30 11:52:37 +0200

    updated missing variable in rg.tf file

--- main/rg.tf
+++ main/rg.tf
@@ -1,16 +1,17 @@
-module "rg" {
+module "eu_gl_rg" {
   source              = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-rg//module?ref=v0.0.4"
-  resource_group_name = var.resource_group_name1
-  resource_group_location = var.resource_group_location1
+  resource_group_name = var.resource_group_name_eu
+  resource_group_location = var.resource_group_location_eu
 }
 
-module "rg" {
+module "us_gl_rg" {
   source              = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-rg//module?ref=v0.0.4"
-  resource_group_name = var.resource_group_name2
-  resource_group_location = var.resource_group_location2
+  resource_group_name = var.resource_group_name_us
+  resource_group_location = var.resource_group_location_us
+}
 
-module "rg" {
+module "au_gl_rg" {
   source              = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-rg//module?ref=v0.0.4"
-  resource_group_name = var.resource_group_name3
-  resource_group_location = var.resource_group_locatio3
+  resource_group_name = var.resource_group_name_au
+  resource_group_location = var.resource_group_location_au
 }
