05fe43d8aa5cece82988876ab94e1dc007a5f7da
Chirag Panchal <chirag.panchal@electrolux.com>
2023-08-30 15:32:23 +0200

    added and updated two more app gateway

--- data/values_test.tfvars
+++ data/values_test.tfvars
@@ -8,20 +8,29 @@ virtual_network_name_eu    = "VN-GL-ELX-EU-Sandbox"
 vnet_address_space_eu      = ["10.216.0.0/16"]
 subnet_address_space_eu    = ["10.216.3.0/24", "10.216.2.192/27", "10.216.2.0/25"]
 
-appgw_pip_name       = "AAG-GL-NonProd-PIP"
-appgw_pip_allocation = "Static"
-appgw_name           = "AppGw-GL-ELX-EU-Sandbox"
-appgw_sku_name       = "WAF_v2"
-appgw_sku_tier       = "WAF_v2"
-appgw_sku_capacity   = 2
-appgw_ip_config_name = "AppGw-GL-IP-Config"
-subnet_id = "/subscriptions/f28071b5-e402-4c1a-83cc-ed0744ce8e0a/resourceGroups/RG-GL-ELX-EU-Sandbox/providers/Microsoft.Network/virtualNetworks/VN-GL-ELX-EU-Sandbox/subnets/Subnet-GW"
+appgw_pip_name_eu       = "AAG-GL-ELX-EU-NonProd-PIP"
+appgw_pip_allocation_eu = "Static"
+appgw_name_eu           = "AppGw-GL-ELX-EU-Sandbox"
+appgw_sku_name_eu       = "WAF_v2"
+appgw_sku_tier_eu      = "WAF_v2"
+appgw_sku_capacity_eu  = 2
+appgw_ip_config_name_eu = "AppGw-GL-ELX-EU-IP-Config"
+subnet_id_eu = "/subscriptions/f28071b5-e402-4c1a-83cc-ed0744ce8e0a/resourceGroups/RG-GL-ELX-EU-Sandbox/providers/Microsoft.Network/virtualNetworks/VN-GL-ELX-EU-Sandbox/subnets/Subnet-GW"
 #----------------------------Resource Group Module Details for US region ------------------------------
 resource_group_location_us = "East US"
 resource_group_name_us     = "RG-GL-ELX-US-Sandbox"
 virtual_network_name_us    = "VN-GL-ELX-US-Sandbox"
 vnet_address_space_us      = ["10.217.0.0/16"]
 subnet_address_space_us    = ["10.217.3.0/24", "10.217.2.192/27", "10.217.2.0/25"]
+
+appgw_pip_name_us       = "AAG-GL-ELX-US-NonProd-PIP"
+appgw_pip_allocation_us  = "Static"
+appgw_name_us_us            = "AppGw-GL-ELX-US-Sandbox"
+appgw_sku_name_us        = "WAF_v2"
+appgw_sku_tier_us       = "WAF_v2"
+appgw_sku_capacity_us   = 2
+appgw_ip_config_name_us = "AppGw-GL-ELX-US-IP-Config"
+subnet_id_us = "/subscriptions/f28071b5-e402-4c1a-83cc-ed0744ce8e0a/resourceGroups/RG-GL-ELX-US-Sandbox/providers/Microsoft.Network/virtualNetworks/VN-GL-ELX-US-Sandbox/subnets/Subnet-GW"
 # ------------------------Resource Group Module Details for AS region------------------------------
 
 resource_group_location_au = "Australia East"
@@ -29,6 +38,16 @@ resource_group_name_au     = "RG-GL-ELX-AU-Sandbox"
 virtual_network_name_au    = "VN-GL-ELX-AU-Sandbox"
 vnet_address_space_au      = ["10.218.0.0/16"]
 subnet_address_space_au    = ["10.218.3.0/24", "10.218.2.192/27", "10.218.2.0/25"]
+
+appgw_pip_name_au       = "AAG-GL-ELX-AU-NonProd-PIP"
+appgw_pip_allocation_au  = "Static"
+appgw_name_us_au           = "AppGw-GL-ELX-AU-Sandbox"
+appgw_sku_name_au       = "WAF_v2"
+appgw_sku_tier_au      = "WAF_v2"
+appgw_sku_capacity_au   = 2
+appgw_ip_config_name_au = "AppGw-GL-ELX-AU-IP-Config"
+subnet_id_au = "/subscriptions/f28071b5-e402-4c1a-83cc-ed0744ce8e0a/resourceGroups/RG-GL-ELX-AU-Sandbox/providers/Microsoft.Network/virtualNetworks/VN-GL-ELX-AU-Sandbox/subnets/Subnet-GW"
+
 #------------------------------------------------------------------------------------------
 subnet_name       = ["Subnet-APIM", "Subnet-GW", "Subnet-AKS"]
 service_endpoints = ["Microsoft.Sql", "Microsoft.KeyVault"]
--- main/appgw.tf
+++ main/appgw.tf
@@ -1,17 +1,66 @@
-module "gl_appgw" {
+module "eu_gl_appgw" {
   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-AppGw//module?ref=v0.0.3"
   tenant_id               = var.tenant_id
   subscription_id         = var.subscription_id
   resource_group_name     = var.resource_group_name_eu
   resource_group_location = var.resource_group_location_eu
-  appgw_pip_name          = var.appgw_pip_name
-  appgw_pip_allocation    = var.appgw_pip_allocation
-  appgw_name              = var.appgw_name
-  appgw_sku_name          = var.appgw_sku_name
-  appgw_sku_tier          = var.appgw_sku_tier
-  appgw_sku_capacity      = var.appgw_sku_capacity
-  appgw_ip_config_name    = var.appgw_ip_config_name
-  subnet_id               = var.subnet_id #module.eu_gl_virtualNetwork.subnet_name[1]
+  appgw_pip_name          = var.appgw_pip_name_eu
+  appgw_pip_allocation    = var.appgw_pip_allocation_eu
+  appgw_name              = var.appgw_name_eu
+  appgw_sku_name          = var.appgw_sku_name_eu
+  appgw_sku_tier          = var.appgw_sku_tier_eu
+  appgw_sku_capacity      = var.appgw_sku_capacity_eu
+  appgw_ip_config_name    = var.appgw_ip_config_name_eu
+  subnet_id               = var.subnet_id_eu #module.eu_gl_virtualNetwork.subnet_name[1]
+  #WAF Configurations
+  waf_enabled                  = var.waf_enabled
+  waf_firewall_mode            = var.waf_firewall_mode
+  waf_rule_set_type            = var.waf_rule_set_type
+  waf_rule_set_version         = var.waf_rule_set_version
+  waf_file_upload_limit_mb     = var.waf_file_upload_limit_mb
+  waf_request_body_check       = var.waf_request_body_check
+  waf_max_request_body_size_kb = var.waf_max_request_body_size_kb
+}
+
+
+module "us_gl_appgw" {
+  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-AppGw//module?ref=v0.0.3"
+  tenant_id               = var.tenant_id
+  subscription_id         = var.subscription_id
+  resource_group_name     = var.resource_group_name_us
+  resource_group_location = var.resource_group_location_us
+  appgw_pip_name          = var.appgw_pip_name_us
+  appgw_pip_allocation    = var.appgw_pip_allocation_us
+  appgw_name              = var.appgw_name_us
+  appgw_sku_name          = var.appgw_sku_name_us
+  appgw_sku_tier          = var.appgw_sku_tier_us
+  appgw_sku_capacity      = var.appgw_sku_capacity_us
+  appgw_ip_config_name    = var.appgw_ip_config_name_us
+  subnet_id               = var.subnet_id_us #module.eu_gl_virtualNetwork.subnet_name[1]
+  #WAF Configurations
+  waf_enabled                  = var.waf_enabled
+  waf_firewall_mode            = var.waf_firewall_mode
+  waf_rule_set_type            = var.waf_rule_set_type
+  waf_rule_set_version         = var.waf_rule_set_version
+  waf_file_upload_limit_mb     = var.waf_file_upload_limit_mb
+  waf_request_body_check       = var.waf_request_body_check
+  waf_max_request_body_size_kb = var.waf_max_request_body_size_kb
+}
+
+module "au_gl_appgw" {
+  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-AppGw//module?ref=v0.0.3"
+  tenant_id               = var.tenant_id
+  subscription_id         = var.subscription_id
+  resource_group_name     = var.resource_group_name_au
+  resource_group_location = var.resource_group_location_au
+  appgw_pip_name          = var.appgw_pip_name_au
+  appgw_pip_allocation    = var.appgw_pip_allocation_au
+  appgw_name              = var.appgw_name_au
+  appgw_sku_name          = var.appgw_sku_name_au
+  appgw_sku_tier          = var.appgw_sku_tier_au
+  appgw_sku_capacity      = var.appgw_sku_capacity_au
+  appgw_ip_config_name    = var.appgw_ip_config_name_au
+  subnet_id               = var.subnet_id_au #module.eu_gl_virtualNetwork.subnet_name[1]
   #WAF Configurations
   waf_enabled                  = var.waf_enabled
   waf_firewall_mode            = var.waf_firewall_mode
