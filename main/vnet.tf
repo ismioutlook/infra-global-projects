73cd9dc3958483ac1895f6fb0f85b0bfe0420c85
Patreddy Reddy <patreddy.reddy@ibm.com>
2023-08-30 11:40:39 +0530

    made changes to rg.tf and vnet.tf

--- main/vnet.tf
+++ main/vnet.tf
@@ -1,8 +1,3 @@
-module "rg" {
-  source              = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-rg//module?ref=v0.0.3"
-  resource_group_name = var.resource_group_name
-}
-
 module "gl_virtualNetwork" {
   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-vnet//module?ref=v0.0.3"
   resource_group_name     = var.resource_group_name
