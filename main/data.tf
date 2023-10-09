2be3341783f825ab1526a5446f456edd94f1f6aa
Chirag Panchal <chirag.panchal@electrolux.com>
2023-10-09 15:55:59 +0200

    commented apim data

--- main/data.tf
+++ main/data.tf
@@ -10,17 +10,17 @@ data "azurerm_subnet" "subnet_gw_us" {
   resource_group_name  = var.us_vars.resource_group.resource_group_location
 }
 
-data "azurerm_subnet" "subnet_apim_eu" {
-  name                 = "APIM-SBT-01"
-  virtual_network_name = "VN-ELX-GL-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.eu_vars.resource_group.resource_group_location
-}
+# data "azurerm_subnet" "subnet_apim_eu" {
+#   name                 = "APIM-SBT-01"
+#   virtual_network_name = "VN-ELX-GL-Concent-NonProd-Spoke-001"
+#   resource_group_name  = var.eu_vars.resource_group.resource_group_location
+# }
 
-data "azurerm_subnet" "subnet_apim_us" {
-  name                 = "APIM-SBT-01"
-  virtual_network_name = "VN-ELX-EUS-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.us_vars.resource_group.resource_group_location
-}
+# data "azurerm_subnet" "subnet_apim_us" {
+#   name                 = "APIM-SBT-01"
+#   virtual_network_name = "VN-ELX-EUS-Concent-NonProd-Spoke-001"
+#   resource_group_name  = var.us_vars.resource_group.resource_group_location
+# }
 
 data "azurerm_subnet" "subnet_aks_eu" {
   name                 = "AKS-SBT-01"
