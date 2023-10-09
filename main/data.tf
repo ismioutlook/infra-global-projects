26392d812a7ae96722d8c6cf2d5c03c851218c3e
Chirag Panchal <chirag.panchal@electrolux.com>
2023-10-09 15:46:21 +0200

    updated var value based on grouping assign

--- main/data.tf
+++ main/data.tf
@@ -1,35 +1,35 @@
 data "azurerm_subnet" "subnet_gw_eu" {
   name                 = "AppGW-SBT-01"
   virtual_network_name = "VN-ELX-GL-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_eu
+  resource_group_name  = var.eu_vars.resource_group.resource_group_location
 }
 
 data "azurerm_subnet" "subnet_gw_us" {
   name                 = "AppGW-SBT-01"
   virtual_network_name = "VN-ELX-EUS-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_us
+  resource_group_name  = var.us_vars.resource_group.resource_group_location
 }
 
 data "azurerm_subnet" "subnet_apim_eu" {
   name                 = "APIM-SBT-01"
   virtual_network_name = "VN-ELX-GL-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_eu
+  resource_group_name  = var.eu_vars.resource_group.resource_group_location
 }
 
 data "azurerm_subnet" "subnet_apim_us" {
   name                 = "APIM-SBT-01"
   virtual_network_name = "VN-ELX-EUS-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_us
+  resource_group_name  = var.us_vars.resource_group.resource_group_location
 }
 
 data "azurerm_subnet" "subnet_aks_eu" {
   name                 = "AKS-SBT-01"
   virtual_network_name = "VN-ELX-GL-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_eu
+  resource_group_name  = var.eu_vars.resource_group.resource_group_location
 }
 
 data "azurerm_subnet" "subnet_aks_us" {
   name                 = "AKS-SBT-01"
   virtual_network_name = "VN-ELX-EUS-Concent-NonProd-Spoke-001"
-  resource_group_name  = var.resource_group_location_us
+  resource_group_name  = var.us_vars.resource_group.resource_group_location
 }
