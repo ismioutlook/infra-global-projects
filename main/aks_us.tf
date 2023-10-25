2fa68f488b43a53d20f3da09e0e4104649c71856
Chirag Panchal <chirag.panchal@electrolux.com>
2023-10-25 14:35:20 +0200

    adding nsg for us

--- main/aks_us.tf
+++ main/aks_us.tf
@@ -1,5 +1,5 @@
 # module "us_gl_aks" {
-#   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-aks//module?ref=v0.0.10"
+#   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-aks//module?ref=v0.0.11"
 #   resource_group_name     = var.us_vars.resource_group.resource_group_name
 #   resource_group_location = var.us_vars.resource_group.resource_group_location
 #   log_analytics_name      = var.aks_vars.azure_k8s_service.log_analytics_name
