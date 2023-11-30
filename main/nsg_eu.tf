3c6ef28512b39a2e7fe84411a86862fc6792a5d3
Chirag Panchal <chirag.panchal@electrolux.com>
2023-11-30 14:06:59 +0100

    added depends on

--- main/nsg_eu.tf
+++ main/nsg_eu.tf
@@ -294,6 +294,7 @@ resource "azurerm_network_security_group" "nsg_apim" {
     source_address_prefix      = "VirtualNetwork"
     source_port_range          = "*"
   }
+  depends_on = [ module.eu_gl_rg ]
 }
 
 resource "azurerm_network_security_group" "nsg_apim_gw" {
@@ -445,4 +446,5 @@ resource "azurerm_network_security_group" "nsg_apim_gw" {
     source_address_prefixes    = ["194.246.0.0/19", "62.252.197.0/24", "217.169.51.0/24", "204.17.245.4"]
     source_port_range          = "*"
   }
+  depends_on = [ module.eu_gl_rg ]
 }
