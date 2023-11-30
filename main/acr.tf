98f4a9ec4a27f8fcb20861fef4af4a5a2c9e1c69
Chirag Panchal <chirag.panchal@electrolux.com>
2023-11-30 13:48:39 +0100

    enable acr and nsg

--- main/acr.tf
+++ main/acr.tf
@@ -1,7 +1,7 @@
-# resource "azurerm_container_registry" "acr" {
-#   name                = var.eu_vars.acr.acr_name
-#   resource_group_name = var.eu_vars.resource_group.resource_group_name
-#   location            = var.eu_vars.resource_group.resource_group_location
-#   sku                 = var.eu_vars.acr.acr_sku
-#   admin_enabled       = var.eu_vars.acr.acr_admin_enabled
-# }
+resource "azurerm_container_registry" "acr" {
+  name                = var.eu_vars.acr.acr_name
+  resource_group_name = var.eu_vars.resource_group.resource_group_name
+  location            = var.eu_vars.resource_group.resource_group_location
+  sku                 = var.eu_vars.acr.acr_sku
+  admin_enabled       = var.eu_vars.acr.acr_admin_enabled
+}
