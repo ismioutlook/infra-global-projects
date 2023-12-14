513c43f1dc0c24867b2030241c56be2d6a5bfe80
Chirag Panchal <chirag.panchal@electrolux.com>
2023-12-14 13:21:45 +0100

    final cleanup

--- main/rg_apac.tf
+++
@@ -1,5 +0,0 @@
-# module "apac_gl_rg" {
-#   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-rg//module?ref=v0.0.4"
-#   resource_group_name     = var.apac_vars.resource_group.resource_group_name
-#   resource_group_location = var.apac_vars.resource_group.resource_group_location
-# }
