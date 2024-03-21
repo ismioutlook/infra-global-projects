29d77231fa3e7da2f2b1d332b2f8f6622c0a5292
Kamran Manzoor <kamran.manzoor@ibm.com>
2024-03-22 00:07:02 +0100

    fix: remove duplicate rg

--- concent-claims/provider.tf
+++ concent-claims/provider.tf
@@ -15,13 +15,3 @@ provider "azurerm" {
 
   }
 }
-
-resource "azurerm_resource_group" "rg" {
-  count    = var.enabled ? 1 : 0
-  name     = "atlantis-controlled-product-${terraform.workspace}-rg"
-  location = "westeurope"
-  tags = {
-    environment = "${terraform.workspace}"
-    purpose     = "atlantis-poc-via-sandbox"
-  }
-}
