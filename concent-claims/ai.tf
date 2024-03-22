3f1e60424e4dfe5b88c9c086abdb4100026615d2
Kamran Manzoor <kamran.manzoor@ibm.com>
2024-03-22 11:06:44 +0100

    fix: bump the module version

--- concent-claims/ai.tf
+++ concent-claims/ai.tf
@@ -1,6 +1,6 @@
 module "app_insights" {
   count                   = var.enabled ? 1 : 0
-  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-appinsights//module?ref=v0.0.2"
+  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-appinsights//module?ref=v0.0.3"
   resource_group_location = azurerm_resource_group.rg[0].location
   tenant_id               = local.tenant_id
   subscription_id         = local.subscription_id
@@ -8,5 +8,4 @@ module "app_insights" {
   app_insights_type       = var.app_insights_type
   app_insights_name       = var.app_insights_name
   workspace_id            = module.law[0].workspace_id
-  depends_on              = [module.law]
 }
