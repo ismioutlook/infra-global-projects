c522e48852b5904beb1080d2d92e6f4ec004c219
mohamed.khan <mohamed.khan@electrolux.com>
2024-04-08 22:03:23 +0530

    keyvault tag version changed to v0.0.2

--- concent-claims/kv.tf
+++ concent-claims/kv.tf
@@ -1,11 +1,11 @@
 module "kv" {
   count                      = var.enabled ? 1 : 0
-  source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.1"
+  source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.2"
   tenant_id                  = local.tenant_id
   resource_group_name        = azurerm_resource_group.rg[0].name
   resource_group_location    = azurerm_resource_group.rg[0].location
   key_vault_name             = var.key_vault_name
   soft_delete_retention_days = var.soft_delete_retention_days
   kv_sku_name                = var.kv_sku_name
-  access_policy              = var.access_policies
+  access_policies            = var.access_policies
 }
