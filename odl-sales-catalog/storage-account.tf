ce8515edb033e2f6e2da8fa8c6e392be291d0e52
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-12 17:38:46 +0300

    feat: adding a storage container for the storage account. CCTGLOBAL-24523

--- odl-sales-catalog/storage-account.tf
+++ odl-sales-catalog/storage-account.tf
@@ -1,4 +1,4 @@
-resource "azurerm_storage_account" "sales-catalog-ingestion-sa" {
+resource "azurerm_storage_account" "sales-catalog-ingestion" {
   count                    = var.enabled ? 1 : 0
   name                     = var.storage_account_name
   resource_group_name      = var.resource_group_name
