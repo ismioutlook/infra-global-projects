b7782b4935baa8634f11d6debea10287750abe7e
Venkata Mummadi <venkata.mummadi@electrolux.com>
2024-04-15 09:45:55 +0000

    added type list(Object) in variable declation

--- concent-claims/variables.tf
+++ concent-claims/variables.tf
@@ -90,13 +90,15 @@ variable "kv_sku_name" {
 }
 
 variable "kv_policy_objects_ids" {
-  type = object({
+  type = list(object({
     reader_obj_ids = list(string)
     admin_obj_ids  = list(string)
-  })
-  default = {
-    reader_obj_ids = []
-    admin_obj_ids  = []
-  }
+  }))
+  default = [
+    {
+      reader_obj_ids = []
+      admin_obj_ids  = []
+    }
+  ]
   description = "List of Keyvault access policies"
 }
