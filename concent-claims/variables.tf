b531254f8e72300139f6f4a35a31353c0219c7eb
Venkata Mummadi <venkata.mummadi@electrolux.com>
2024-04-15 10:48:35 +0000

    removed default value.

--- concent-claims/variables.tf
+++ concent-claims/variables.tf
@@ -90,14 +90,10 @@ variable "kv_sku_name" {
 }
 
 variable "kv_policy_objects_ids" {
-  type = list(object({
+  type = object({
     reader_obj_ids = list(string)
     admin_obj_ids  = list(string)
-  }))
-  default = {
-    reader_obj_ids = []
-    admin_obj_ids  = []
-  }
+  })
   description = "List of Keyvault access policies"
 }
 
