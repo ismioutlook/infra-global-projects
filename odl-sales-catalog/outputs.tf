245920e7a986bfd6b3666c6c742cff6e0b7978c8
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-16 17:34:18 +0300

    feat: changing the configuration a bit to access storeage account access key by the function app

--- odl-sales-catalog/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "storage_account_access_key" {
-  value       = azurerm_storage_account.sales-catalog-ingestion[count.index].storage_account_access_key
-  description = "Access key for the storage account"
-}
