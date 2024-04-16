f89336baf4ea006909e53eb232d5727a1cd552d9
devusertpt <venkata.mummadi@electrolux.com>
2024-04-16 14:50:50 +0530

    Formatting th tf files

--- concent-claims/locals.tf
+++ concent-claims/locals.tf
@@ -3,9 +3,9 @@ locals {
     environment = "${terraform.workspace}"
     domain      = "concent-claims"
   }
-  tenant_id       = data.azurerm_subscription.current.tenant_id
-  subscription_id = data.azurerm_subscription.current.subscription_id
+  tenant_id             = data.azurerm_subscription.current.tenant_id
+  subscription_id       = data.azurerm_subscription.current.subscription_id
   kv_reader_objects_ids = var.enabled ? concat(var.kv_policy_objects_ids.reader_objects_ids, module.data_factory[0].adf_identity[*].principal_id) : []
   # kv_reader_objects_ids = var.enabled ? var.kv_policy_objects_ids.reader_objects_ids : []
-  kv_admin_objects_ids  = var.enabled ? var.kv_policy_objects_ids.admin_objects_ids : []
+  kv_admin_objects_ids = var.enabled ? var.kv_policy_objects_ids.admin_objects_ids : []
 }
