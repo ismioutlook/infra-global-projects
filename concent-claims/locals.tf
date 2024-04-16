7d57a5e79ce11f3ad240fcc39839a8300de2ef96
Venkata Mummadi <venkata.mummadi@electrolux.com>
2024-04-16 09:45:41 +0000

    removed Commented Line

--- concent-claims/locals.tf
+++ concent-claims/locals.tf
@@ -6,6 +6,5 @@ locals {
   tenant_id             = data.azurerm_subscription.current.tenant_id
   subscription_id       = data.azurerm_subscription.current.subscription_id
   kv_reader_objects_ids = var.enabled ? concat(var.kv_policy_objects_ids.reader_objects_ids, module.data_factory[0].adf_identity[*].principal_id) : []
-  # kv_reader_objects_ids = var.enabled ? var.kv_policy_objects_ids.reader_objects_ids : []
   kv_admin_objects_ids = var.enabled ? var.kv_policy_objects_ids.admin_objects_ids : []
 }
