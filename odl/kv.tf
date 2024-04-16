a486b705d7ad239b7bd34cf85c63a7e47a559b95
Mohamed Khan <mohamed.khan@electrolux.com>
2024-04-16 15:43:36 +0000

    keyvault variables applied to module kv

--- odl/kv.tf
+++ odl/kv.tf
@@ -1,8 +1,8 @@
 module "kv" {
   source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.1"
   tenant_id                  = var.tenant_id
-  resource_group_name        = var.resource_group_name
-  resource_group_location    = var.resource_group_location
+  resource_group_name        = var.kv_resource_group_name
+  resource_group_location    = var.kv_resource_group_location
   key_vault_name             = var.key_vault_name
   soft_delete_retention_days = var.soft_delete_retention_days
   kv_sku_name                = var.kv_sku_name
