1e185a167e23a2669fb531589268357d56d7f285
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-17 12:08:17 +0300

    feat: changing azurerm_function_app to azurerm_linux_function_app

--- odl-sales-catalog/function-app.tf
+++ odl-sales-catalog/function-app.tf
@@ -1,12 +1,16 @@
-resource "azurerm_function_app" "sales-catalog-ingestion-fap" {
+resource "azurerm_linux_function_app" "sales-catalog-ingestion-fap" {
   count                      = var.enabled ? 1 : 0
   name                       = var.function_app_name
   location                   = var.resource_group_location
   resource_group_name        = var.resource_group_name
-  app_service_plan_id        = var.service_plan_name
+  service_plan_id            = var.service_plan_name
   storage_account_name       = var.storage_account_name
   storage_account_access_key = azurerm_storage_account.sales-catalog-ingestion[count.index].primary_access_key
   tags                       = local.tags
+
+  site_config {
+    linux_fx_version = "PYTHON|3.11"
+  }
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "python"
