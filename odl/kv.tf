44ba65138f426f67e96a84b612cbbb1e35e465a6
mohamed.khan <mohamed.khan@electrolux.com>
2024-04-17 09:32:23 +0530

    keyvault keys added for dev and 1int

--- odl/kv.tf
+++ odl/kv.tf
@@ -1,5 +1,5 @@
 module "kv" {
-  source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.1"
+  source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.2"
   tenant_id                  = var.tenant_id
   resource_group_name        = var.kv_resource_group_name
   resource_group_location    = var.kv_resource_group_location
@@ -7,4 +7,55 @@ module "kv" {
   soft_delete_retention_days = var.soft_delete_retention_days
   kv_sku_name                = var.kv_sku_name
 
+}
+
+resource "azurerm_key_vault_key" "odl-sops-dev" {
+  name         = "odl-sops-dev"
+  key_vault_id = module.kv.key_vault_id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
+}
+
+
+resource "azurerm_key_vault_key" "odl-sops-1int" {
+  name         = "odl-sops-1int"
+  key_vault_id = module.kv.key_vault_id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+
+  rotation_policy {
+    automatic {
+      time_before_expiry = "P30D"
+    }
+
+    expire_after         = "P90D"
+    notify_before_expiry = "P29D"
+  }
 }
