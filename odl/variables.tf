d5e2e334d9e08ce1dfde74d19cac0f9211d0e843
mohamed.khan <mohamed.khan@electrolux.com>
2024-04-17 11:16:20 +0530

    keyvault variables added

--- odl/variables.tf
+++ odl/variables.tf
@@ -23,3 +23,19 @@ variable "kv_resource_group_location" {
   type        = string
   description = "Location of the resource group for Keyvault"
 }
+
+
+#---Keyvault  -----
+
+variable "key_vault_name" {
+  type        = string
+  description = "Name of the Azure Key Vault"
+}
+variable "soft_delete_retention_days" {
+  type        = string
+  description = "kv soft delete retention days"
+}
+variable "kv_sku_name" {
+  type        = string
+  description = "key vault sku name"
+}
