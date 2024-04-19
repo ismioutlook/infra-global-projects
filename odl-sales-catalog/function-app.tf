8f5e8a58a5e74c769d0ff13530ceaa1041ea28d6
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-19 11:42:51 +0300

    feat: adding the mapping between the function app and subnet

--- odl-sales-catalog/function-app.tf
+++ odl-sales-catalog/function-app.tf
@@ -23,3 +23,8 @@ resource "azurerm_linux_function_app" "sales-catalog-ingestion-fap" {
   }
 
 }
+
+resource "azurerm_app_service_virtual_network_swift_connection" "function-app-subnet-map" {
+  app_service_id = azurerm_linux_function_app.sales-catalog-ingestion-fap.id
+  subnet_id      = azurerm_subnet.sc-ingestion-sbnt.id
+}
