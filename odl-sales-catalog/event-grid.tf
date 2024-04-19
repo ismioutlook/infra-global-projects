a1f1b75a87967c231b21f162a75cb78ac91d814d
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-19 14:24:16 +0300

    fix: adding the delegation for a subnet

--- odl-sales-catalog/event-grid.tf
+++ odl-sales-catalog/event-grid.tf
@@ -4,5 +4,5 @@ resource "azurerm_eventgrid_system_topic" "sales-catalog-ingestion-sytp" {
   source_arm_resource_id = azurerm_storage_account.sales-catalog-ingestion[count.index].id
   resource_group_name    = var.resource_group_name
   location               = var.resource_group_location
-  topic_type             = "Microsoft.Storage.StorageAccounts"
+  topic_type             = var.eg_topic_type
 }
