58135de66c3fe8634d08eeeaa0e08db4748bf383
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-23 13:37:53 +0300

    feat: adding azure function creation from zip config. removing subscription

--- odl-sales-catalog/event-grid.tf
+++ odl-sales-catalog/event-grid.tf
@@ -7,26 +7,26 @@ resource "azurerm_eventgrid_system_topic" "sales-catalog-ingestion-sytp" {
   topic_type             = var.eg_topic_type
 }
 
-resource "azurerm_eventgrid_event_subscription" "sales-catalog-ingestion-sbsc" {
-  count                 = var.enabled ? 1 : 0
-  name                  = var.eventgrid_subscription_name
-  scope                 = azurerm_eventgrid_system_topic.sales-catalog-ingestion-sytp[count.index].id
-  event_delivery_schema = var.eventgrid_subscription_event_delivery_schema
+# resource "azurerm_eventgrid_event_subscription" "sales-catalog-ingestion-sbsc" {
+#   count                 = var.enabled ? 1 : 0
+#   name                  = var.eventgrid_subscription_name
+#   scope                 = azurerm_eventgrid_system_topic.sales-catalog-ingestion-sytp[count.index].id
+#   event_delivery_schema = var.eventgrid_subscription_event_delivery_schema
 
-  azure_function_endpoint {
-    function_id = "${azurerm_linux_function_app.sales-catalog-ingestion-fap[count.index].id}/functions/SCEventGridTrigger"
-  }
+#   azure_function_endpoint {
+#     function_id = "${azurerm_linux_function_app.sales-catalog-ingestion-fap[count.index].id}/functions/SCEventGridTrigger"
+#   }
 
-  subject_filter {
-    subject_begins_with = "/blobServices/default/containers/${var.storage_container_name}/blobs/${var.storage_container_rex_upload_folder}"
-    subject_ends_with   = var.eventgrid_subscription_suffix_filter
-  }
+#   subject_filter {
+#     subject_begins_with = "/blobServices/default/containers/${var.storage_container_name}/blobs/${var.storage_container_rex_upload_folder}"
+#     subject_ends_with   = var.eventgrid_subscription_suffix_filter
+#   }
 
-  advanced_filter {
-    string_contains {
-      key    = var.eventgrid_subscription_advanced_filter_key
-      values = var.eventgrid_subscription_advanced_filter_values
-    }
-  }
+#   advanced_filter {
+#     string_contains {
+#       key    = var.eventgrid_subscription_advanced_filter_key
+#       values = var.eventgrid_subscription_advanced_filter_values
+#     }
+#   }
 
-}
+# }
