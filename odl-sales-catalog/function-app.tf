d7b69eabb0ed1c53d4cdc3ef7074d7164fc4864b
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-04-25 13:13:12 +0300

    feat: testing Azure function deployment from the storage account

--- odl-sales-catalog/function-app.tf
+++ odl-sales-catalog/function-app.tf
@@ -6,10 +6,9 @@ resource "azurerm_linux_function_app" "sales-catalog-ingestion-fap" {
   service_plan_id            = var.service_plan_name
   storage_account_name       = var.storage_account_name
   storage_account_access_key = azurerm_storage_account.sales-catalog-ingestion[count.index].primary_access_key
-  # zip_deploy_file            = data.archive_file.function.output_path
-
-  virtual_network_subnet_id = azurerm_subnet.sc-ingestion-sbnt.id
-  tags                      = local.tags
+  zip_deploy_file            = data.archive_file.function.output_path
+  virtual_network_subnet_id  = azurerm_subnet.sc-ingestion-sbnt.id
+  tags                       = local.tags
 
   site_config {
     application_stack {
@@ -18,20 +17,21 @@ resource "azurerm_linux_function_app" "sales-catalog-ingestion-fap" {
   }
 
   app_settings = {
-    "FUNCTIONS_WORKER_RUNTIME"       = "python"
-    "ENABLE_ORYX_BUILD"              = true
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
-    "FUNCTIONS_EXTENSION_VERSION"    = "~4"
+    "FUNCTIONS_WORKER_RUNTIME"       = "python",
+    "ENABLE_ORYX_BUILD"              = true,
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true,
+    "FUNCTIONS_EXTENSION_VERSION"    = "~4",
+    "WEBSITE_RUN_FROM_PACKAGE"       = "https://${azurerm_storage_account.sales-catalog-ingestion.name}.blob.core.windows.net/${azurerm_storage_container.sales-catalog-ingestion-cont.name}/${azurerm_storage_blob.storage_blob.name}${data.azurerm_storage_account_blob_container_sas.storage_account_blob_container_sas.sas}",
   }
 
   depends_on = [data.archive_file.function]
 
-  provisioner "local-exec" {
-    command = <<EOT
-       az functionapp deployment source config-zip
-       -g ${var.resource_group_name} 
-       -n ${var.function_app_name}
-       --src ${data.archive_file.function.output_path}
-  EOT
-  }
+  # provisioner "local-exec" {
+  #   command = <<EOT
+  #      az functionapp deployment source config-zip
+  #      -g ${var.resource_group_name} 
+  #      -n ${var.function_app_name}
+  #      --src ${data.archive_file.function.output_path}
+  # EOT
+  # }
 }
--- odl-sales-catalog/storage-account.tf
+++ odl-sales-catalog/storage-account.tf
@@ -14,3 +14,30 @@ resource "azurerm_storage_container" "sales-catalog-ingestion-cont" {
   storage_account_name  = azurerm_storage_account.sales-catalog-ingestion[count.index].name
   container_access_type = var.storage_container_access_type
 }
+
+resource "azurerm_storage_blob" "storage_blob" {
+  count                  = var.enabled ? 1 : 0
+  name                   = "${filesha256(var.archive_file.output_path)}.zip"
+  storage_account_name   = azurerm_storage_account.sales-catalog-ingestion[count.index].name
+  storage_container_name = azurerm_storage_container.sales-catalog-ingestion-cont[count.index].name
+  type                   = "Block"
+  source                 = var.archive_file.output_path
+}
+
+data "azurerm_storage_account_blob_container_sas" "storage_account_blob_container_sas" {
+  count             = var.enabled ? 1 : 0
+  connection_string = azurerm_storage_account.sales-catalog-ingestion[count.index].primary_connection_string
+  container_name    = azurerm_storage_container.sales-catalog-ingestion-cont[count.index].name
+
+  start  = "2024-04-25T00:00:00Z"
+  expiry = "2025-01-01T00:00:00Z"
+
+  permissions {
+    read   = true
+    add    = false
+    create = false
+    write  = false
+    delete = false
+    list   = false
+  }
+}
