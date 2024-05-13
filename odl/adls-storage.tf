c5cb07078e00aaceb250295611b8a1e7889ee375
mohamed.khan <mohamed.khan@electrolux.com>
2024-05-13 22:21:25 +0530

    ref changed to module for container

--- odl/adls-storage.tf
+++ odl/adls-storage.tf
@@ -18,7 +18,7 @@ module "storage_account" {
 }
 
 module "storage_container" {
-  source                        = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-storage-container//iasc?ref=dev"
+  source                        = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-storage-container//module?ref=dev"
   storage_account_name          = var.storage_account_name
   container_name                = var.storage_container_name
   storage_container_access_type = var.storage_container_access_type
