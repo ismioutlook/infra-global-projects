5a6a3ac7e7ab9cbd06e403bd7f463ffdcc7c17c2
mohamed.khan <mohamed.khan@electrolux.com>
2024-05-13 23:17:19 +0530

    resource group added

--- odl/adls-storage.tf
+++ odl/adls-storage.tf
@@ -1,23 +1,16 @@
-module "storage_account" {
-  source          = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-sa//iasc?ref=v0.0.4"
-  tenant_id       = local.tenant_id
-  subscription_id = local.subscription_id
-
-  resource_group_name     = azurerm_resource_group.rg.name
-  resource_group_location = azurerm_resource_group.rg.location
-
-  storage_account_name        = var.storage_account_name
-  storage_account_tier        = var.storage_account_tier
-  storage_account_replication = var.storage_account_replication
-  account_kind                = var.account_kind
-  is_hns_enabled              = var.is_hns_enabled
-
+resource "azurerm_storage_account" "sa" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = var.storage_account_tier
+  account_replication_type = var.storage_account_replication
+  account_kind             = var.account_kind
+  is_hns_enabled           = var.is_hns_enabled
 }
 
 module "storage_container" {
   source                = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-storage-container//module?ref=dev"
-  storage_account_name  = var.storage_account_name
+  storage_account_name  = azurerm_storage_account.sa.name
   container_name        = var.storage_container_name
   container_access_type = var.storage_container_access_type
-
 }
