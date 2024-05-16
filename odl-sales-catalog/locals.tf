0594edfdf68181dc49ba6c2aaa588f4816c6fde0
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-05-16 18:24:19 +0300

    refactor: adding Function App identity to grant KV access

--- odl-sales-catalog/locals.tf
+++ odl-sales-catalog/locals.tf
@@ -8,7 +8,7 @@ locals {
   subscription_id = data.azurerm_subscription.current.subscription_id #"f28071b5-e402-4c1a-83cc-ed0744ce8e0a" #
   current_user_id = data.azurerm_client_config.current.object_id
   # 75f9a0c1-6e61-4cbe-beba-9ab39034b9a0 ---> CSA-AAD-PRJ-Concent-PE-Admin
-  kv_admin_object_ids = ["75f9a0c1-6e61-4cbe-beba-9ab39034b9a0", local.current_user_id, "212ab792-786b-40d4-9cae-3e21087453f2", azurerm_linux_function_app.sales-catalog-ingestion-fap[0].identity[0].principal_id]
+  kv_admin_object_ids = ["75f9a0c1-6e61-4cbe-beba-9ab39034b9a0", local.current_user_id, "212ab792-786b-40d4-9cae-3e21087453f2", azurerm_linux_function_app.sales-catalog-ingestion-fap[0].identity.principal_id]
   enabled_keyvault    = var.enabled && var.enabled_keyvault ? true : false
   sops_key_name       = format("odl-sopskey-%s", "${terraform.workspace}")
 }
