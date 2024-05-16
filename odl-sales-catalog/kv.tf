305f02fbc42215a4a3eafca2d1ec6a6121ca8498
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-05-16 11:46:15 +0300

    fix: removed tags attribute

--- odl-sales-catalog/kv.tf
+++ odl-sales-catalog/kv.tf
@@ -8,24 +8,23 @@ module "kv" {
   soft_delete_retention_days = var.soft_delete_retention_days
   kv_sku_name                = var.kv_sku_name
   admin_objects_ids          = local.kv_admin_object_ids
-  tags                       = local.tags
 }
 
-resource "azurerm_key_vault_key" "keys" {
-  count        = local.enabled_keyvault ? 1 : 0
-  name         = local.sops_key_name
-  key_vault_id = module.kv[0].key_vault_id
-  key_type     = "RSA"
-  key_size     = 2048
+# resource "azurerm_key_vault_key" "keys" {
+#   count        = local.enabled_keyvault ? 1 : 0
+#   name         = local.sops_key_name
+#   key_vault_id = module.kv[0].key_vault_id
+#   key_type     = "RSA"
+#   key_size     = 2048
 
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
+#   key_opts = [
+#     "decrypt",
+#     "encrypt",
+#     "sign",
+#     "unwrapKey",
+#     "verify",
+#     "wrapKey",
+#   ]
 
-}
+# }
 
