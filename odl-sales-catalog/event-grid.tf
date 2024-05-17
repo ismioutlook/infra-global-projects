d72d4714d4e45809c239bbd20c5ac50d40142bf3
Viktoras Ciumanovas <viktoras.ciumanovas@ibm.com>
2024-05-17 10:29:11 +0300

    fix: mapping the correct storage account with event grid

--- odl-sales-catalog/event-grid.tf
+++ odl-sales-catalog/event-grid.tf
@@ -1,7 +1,7 @@
 resource "azurerm_eventgrid_system_topic" "sales-catalog-ingestion-sytp" {
   count                  = var.enabled ? 1 : 0
   name                   = var.eventgrid_system_topic_name
-  source_arm_resource_id = azurerm_storage_account.sales-catalog-ingestion[count.index].id
+  source_arm_resource_id = azurerm_storage_account.sales-catalog-rex-upload[count.index].id
   resource_group_name    = var.resource_group_name
   location               = var.resource_group_location
   topic_type             = var.eg_topic_type
--- odl-sales-catalog/function-app.tf
+++ odl-sales-catalog/function-app.tf
@@ -34,7 +34,7 @@ resource "azurerm_linux_function_app" "sales-catalog-ingestion-fap" {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.sales-catalog-rex-appin.instrumentation_key
     "sc_rex_upload_conn_str"         = azurerm_storage_account.sales-catalog-rex-upload[count.index].primary_connection_string
     "sc_rex_upload_cont_name"        = var.storage_container_name_rex
-    "kv_uri"                         = "https://$(var.key_vault_name).vault.azure.net"
+    "kv_uri"                         = "https://${var.key_vault_name}.vault.azure.net"
     "kv_secret_name"                 = var.key_vault_secret_name
 
     # "ENABLE_ORYX_BUILD"                        = true,
