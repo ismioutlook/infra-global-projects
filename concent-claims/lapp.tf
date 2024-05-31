323e61b5c74b499b99095c10ee569cfd378faba7
Atul Sharma <atul.sharma@electrolux.com>
2024-05-31 14:12:56 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,4 +1,4 @@
-module "app_insights_1" {
+module "logic_app" {
   count                   = var.enabled ? 1 : 0
   source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-appinsights//module?ref=v0.0.3"
   resource_group_location = azurerm_resource_group.rg[0].location
