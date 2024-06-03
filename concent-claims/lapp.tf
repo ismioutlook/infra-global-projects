6bce7483725f0c113ae1c55a8957319dd3255ae1
Atul Sharma <atul.sharma@electrolux.com>
2024-06-03 15:14:54 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,4 +1,4 @@
 module "logic_app" {
-  count                   = var.enabled ? 1 : 0
-  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-logicapps"
+  count  = var.enabled ? 1 : 0
+  source = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-logicapps"
 }
