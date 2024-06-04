579738f657c149492e64093e27a932804c074a39
Kamran Manzoor <kamran.manzoor@ibm.com>
2024-06-04 14:04:55 +0200

    fix: uncomment the code

--- main/api_gw_role_assignments.tf
+++ main/api_gw_role_assignments.tf
@@ -1,8 +1,8 @@
-# resource "azurerm_role_assignment" "api_gw_rbac" {
-#   for_each = { for item in local.api_gw_flat_role_assign_map : "${item.role}-${item.group}" => item }
+resource "azurerm_role_assignment" "api_gw_rbac" {
+  for_each = { for item in local.api_gw_flat_role_assign_map : "${item.role}-${item.group}" => item }
 
-#   principal_id         = data.azuread_group.ad_groups[each.value.group].object_id
-#   scope                = data.azurerm_application_gateway.app_gateway.id
-#   role_definition_name = each.value.role
-#   principal_type       = "Group"
-# }
+  principal_id         = data.azuread_group.ad_groups[each.value.group].object_id
+  scope                = data.azurerm_application_gateway.app_gateway.id
+  role_definition_name = each.value.role
+  principal_type       = "Group"
+}
