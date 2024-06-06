55a2d112a69965ba5d8ee45e83a607a821dc6527
Atul Sharma <atul.sharma@electrolux.com>
2024-06-06 13:27:54 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,10 +1,32 @@
-module "logic_app" {
-  count                   = var.enabled ? 1 : 0
-  source                  = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-logicapp?ref=v1.3.0"
-  resource_group_location = azurerm_resource_group.rg[0].location
-  tenant_id               = local.tenant_id
-  subscription_id         = local.subscription_id
-  resource_group_name     = azurerm_resource_group.rg[0].name //
-  logic_app_type          = var.logic_app_type
-  logic_app_name          = var.logic_app_name
+resource "azurerm_app_service_plan" "asp" {
+  name                = var.app_service_plan_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  kind                = var.app_kind #"elastic"
+
+
+  sku {
+    tier = var.app_tier #"WorkflowStandard"
+    size = var.app_size #"WS2"
+  }
+}
+
+resource "azurerm_logic_app_standard" "logicapp" {
+  name                       = var.logic_app_name
+  location                   = var.resource_group_location
+  resource_group_name        = var.resource_group_name
+  app_service_plan_id        = azurerm_app_service_plan.asp.id
+  storage_account_name       = data.azurerm_storage_account.sa.name
+  storage_account_access_key = data.azurerm_storage_account.sa.primary_access_key
+
+  app_settings = {
+    "FUNCTIONS_WORKER_RUNTIME"              = "node"
+    "WEBSITE_NODE_DEFAULT_VERSION"          = "~16"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"        = data.azurerm_application_insights.appinsight.instrumentation_key
+    "APPLICATIONINSIGHTS_CONNECTION_STRING" = data.azurerm_application_insights.appinsight.connection_string
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
 }
