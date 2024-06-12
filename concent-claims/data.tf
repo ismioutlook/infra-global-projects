ad50717e977b38bc77799fbdba04548b007b7bb7
Atul Sharma <atul.sharma@electrolux.com>
2024-06-12 12:19:46 +0000

    Updated data.tf

--- concent-claims/data.tf
+++ concent-claims/data.tf
@@ -1,2 +1,15 @@
-data "azurerm_subscription" "current" {
+data "azurerm_subscription" "current" {}
+
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_storage_account" "sa" {
+  name                = var.storage_account_name
+  resource_group_name = var.resource_group_name
+}
+
+data "azurerm_application_insights" "appinsight" {
+  name                = var.app_insight_name
+  resource_group_name = var.resource_group_name
 }
