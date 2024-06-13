0f4062430031d2dd436cc827232e741fa93095d6
Kamran Manzoor <kamran.manzoor@ibm.com>
2024-06-14 01:54:37 +0200

    fix: add the env confs back and use flag to control prov

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,5 +1,5 @@
 resource "azurerm_logic_app_workflow" "logicapp" {
-  count               = var.enabled ? 1 : 0
+  count               = var.enabled && var.enabled_logicapp ? 1 : 0
   name                = var.logic_app_name
   location            = azurerm_resource_group.rg[0].location
   resource_group_name = azurerm_resource_group.rg[0].name
