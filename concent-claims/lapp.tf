482cb4dc4d9e35e24a5563789eceaa888b268dc0
Atul Sharma <atul.sharma@electrolux.com>
2024-06-13 07:11:55 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,8 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = azurerm_resource_group.rg.name
-  location = azurerm_resource_group.rg.location
-}
-
 resource "azurerm_storage_account" "sa" {
   name                     = var.storage_account_name
   resource_group_name      = azurerm_resource_group.rg.name
