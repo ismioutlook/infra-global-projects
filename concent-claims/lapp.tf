3588eee3147d177a7d9da2241c6022e6fb5484fa
Atul Sharma <atul.sharma@electrolux.com>
2024-06-13 15:36:18 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,4 +1,4 @@
-resource "azurerm_logic_app_workflow" "example" {
+resource "azurerm_logic_app_workflow" "logicapp" {
   count               = var.enabled ? 1 : 0
   name                = var.logic_app_name
   location            = azurerm_resource_group.rg[0].location
