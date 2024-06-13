bc4e113f6ef0363daa7fe5a7645234e96a3d39de
Atul Sharma <atul.sharma@electrolux.com>
2024-06-13 11:20:30 +0000

    Updated lapp.tf

--- concent-claims/lapp.tf
+++ concent-claims/lapp.tf
@@ -1,12 +1,3 @@
-resource "azurerm_storage_account" "sa" {
-  count                    = var.enabled ? 1 : 0
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.rg[0].name
-  location                 = azurerm_resource_group.rg[0].location
-  account_tier             = var.storage_account_tier
-  account_replication_type = var.storage_account_replication
-}
-
 resource "azurerm_service_plan" "asp" {
   count               = var.enabled ? 1 : 0
   name                = var.app_service_plan_name
@@ -22,8 +13,8 @@ resource "azurerm_logic_app_standard" "logicapp" {
   location                   = azurerm_resource_group.rg[0].location
   resource_group_name        = azurerm_resource_group.rg[0].name
   app_service_plan_id        = azurerm_service_plan.asp[0].id
-  storage_account_name       = azurerm_storage_account.sa[0].name
-  storage_account_access_key = azurerm_storage_account.sa[0].primary_access_key
+  storage_account_name       = data.azurerm_storage_account.sa.name
+  storage_account_access_key = data.azurerm_storage_account.sa.primary_access_key
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME"     = "node"
