9410a64cbe66c99d2d50c4dd9bc816b537510855
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-07-24 15:39:52 +0300

    add b2b portal infra

---
+++ terraform/concent-claims/b2b-portal/kv.tf
@@ -0,0 +1,14 @@
+# module "kv" {
+#   count                      = var.enabled ? 1 : 0
+#   source                     = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-keyvault//module?ref=v0.0.3"
+#   tenant_id                  = local.tenant_id
+#   resource_group_name        = azurerm_resource_group.rg[0].name
+#   resource_group_location    = azurerm_resource_group.rg[0].location
+#   key_vault_name             = var.key_vault_name
+#   soft_delete_retention_days = var.soft_delete_retention_days
+#   kv_sku_name                = var.kv_sku_name
+#   reader_objects_ids         = local.kv_reader_objects_ids
+#   admin_objects_ids          = local.kv_admin_objects_ids
+# }
+
+
---
+++ terraform/concent-claims/b2b-portal/sa.tf
@@ -0,0 +1,24 @@
+# module "sa" {
+#   count                       = var.enabled ? 1 : 0
+#   source                      = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-sa//module/sa?ref=v0.0.1"
+#   resource_group_location     = azurerm_resource_group.rg[0].location
+#   tenant_id                   = local.tenant_id
+#   subscription_id             = local.subscription_id
+#   resource_group_name         = azurerm_resource_group.rg[0].name
+#   storage_account_name        = var.storage_account_name
+#   storage_account_tier        = var.storage_account_tier
+#   storage_account_replication = var.storage_account_replication
+# }
+
+# module "sa_ctr" {
+#   count                         = var.enabled ? 1 : 0
+#   source                        = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-sa//module/sa_ctr?ref=v0.0.1"
+#   resource_group_location       = var.resource_group_location
+#   tenant_id                     = data.azurerm_subscription.current.tenant_id
+#   subscription_id               = data.azurerm_subscription.current.subscription_id
+#   resource_group_name           = var.resource_group_name
+#   storage_account_name          = var.storage_account_name
+#   storage_container_name        = var.storage_container_name
+#   storage_container_access_type = var.storage_container_access_type
+#   depends_on                    = [module.sa]
+# }
