3315875afbb5895a2230118440b7a12888a0a86a
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-07-29 14:47:31 +0300

    add b2b portal

--- terraform/concent-claims/b2b-portal/lapp.tf
+++ terraform/concent-claims/b2b-portal/lapp.tf
@@ -4,7 +4,8 @@ data "azurerm_storage_account" "sa" {
   depends_on          = [module.sa]
 }
 module "logic_apps" {
-  source   = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-logicapp?ref=logicapp-prod-r2d2-refactor"
+  count    = var.enabled ? 1 : 0
+  source   = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-mod-logicapp?ref=d1a305d4692fe77d32e6c1d3d7a998109af3fd42"
   rg_name  = module.rg[0].name
   location = module.rg[0].location
   tags     = {}
