28a6593a41d9f0b2d29c02119bb876737789e07c
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-07-29 11:14:58 +0300

    add b2b resources

--- terraform/concent-claims/b2b-portal/lapp.tf
+++ terraform/concent-claims/b2b-portal/lapp.tf
@@ -11,7 +11,7 @@ module "logic_apps" {
 
   logic_apps = [
     {
-      name                       = var.logic_app_name
+      name                       = module.logic_apps.logic_apps[0].name
       app_service_plan_name      = var.app_service_plan_name
       os_type                    = var.app_service_plan_name_os_type
       sku_name                   = var.app_service_plan_name_sku_name
