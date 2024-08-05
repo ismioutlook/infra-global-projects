737a6c29502e9c8b8ee70812e17fb11aed6885f1
mohamed.khan <mohamed.khan@electrolux.com>
2024-08-05 20:36:10 +0530

    keyvault  secret storage disabled

--- terraform/odl/odl-sales-catalog/data.tf
+++ terraform/odl/odl-sales-catalog/data.tf
@@ -16,8 +16,8 @@ data "azurerm_subnet" "subnet" {
   resource_group_name  = var.function_app_subnet_details.resource_group_name
 }
 
-data "azurerm_key_vault_secret" "eventsub_delivery_secret" {
-  count        = var.enabled && length(var.eventgrid_topics) > 0 ? 1 : 0
-  name         = local.sales_catalog_app_services_api_key_secret
-  key_vault_id = var.keyvault_id
-}
+# data "azurerm_key_vault_secret" "eventsub_delivery_secret" {
+#   count        = var.enabled && length(var.eventgrid_topics) > 0 ? 1 : 0
+#   name         = local.sales_catalog_app_services_api_key_secret
+#   key_vault_id = var.keyvault_id
+# }
