bf684f75e5a2119c38bb3c7ca957b5965fcfaaea
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-08-08 16:58:08 +0300

    add c4c replica infra

--- terraform/c4c-replica-md/provider.tf
+++ terraform/c4c-replica-md/provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.94.0"
+      version = "~>3.108.0"
     }
   }
   backend "azurerm" {
-    key = "b2b-portal/terraform.tfstate"
+    key = "c4c-replica-md/terraform.tfstate"
   }
 }
 
