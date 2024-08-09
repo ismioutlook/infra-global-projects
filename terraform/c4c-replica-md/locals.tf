6fb6e0a90b145faf11054bc0c0209a01db7c2514
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-08-09 15:18:53 +0300

    add infra resources

--- terraform/c4c-replica-md/locals.tf
+++ terraform/c4c-replica-md/locals.tf
@@ -6,7 +6,7 @@ locals {
   tenant_id       = data.azurerm_subscription.current.tenant_id
   subscription_id = data.azurerm_subscription.current.subscription_id
   tenant_id_map   = { tenant_id = "${local.tenant_id}" }
-  kv_adf_roleassignment = {
+  kv_roleassignment = {
     secrets_officer = {
       role_definition_id_or_name = "Key Vault Secrets Officer"
       principal_id               = "${module.adf[0].data_factory_managed_identity}"
