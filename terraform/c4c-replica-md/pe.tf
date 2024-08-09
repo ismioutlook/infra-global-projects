a44a61aa39bf5a74d5725e97659482195c3fe87e
Mennatallah Abdallah Mostafa Ali <mennatallah.ammar@ibm.com>
2024-08-09 13:34:46 +0300

    add infra resources

--- terraform/c4c-replica-md/pe.tf
+++ terraform/c4c-replica-md/pe.tf
@@ -1,5 +1,6 @@
 module "adf-pe" {
   count          = var.enabled ? 1 : 0
+  name           = "pl-VN-ELX-GL-Concent-NonProd-Spoke-001-01"
   source         = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-module-pe?ref=496689fbec74fa3fe375f89d45d6bd47d8a8dbbe"
   resource_group = module.rg[0].name
   location       = module.rg[0].location
@@ -20,6 +21,7 @@ module "adf-pe" {
 
 module "sql-pe" {
   source         = "git@ssh.dev.azure.com:v3/ELX-Marketing-DevOps/infra-modules/infra-module-pe?ref=496689fbec74fa3fe375f89d45d6bd47d8a8dbbe"
+  name           = "pl-VN-ELX-GL-Concent-NonProd-Spoke-001-02"
   resource_group = module.rg[0].name
   location       = module.rg[0].location
   private_dns_zone = {
